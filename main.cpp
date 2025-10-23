1a7352ca1a8c5ca81290edcc265fdfd84289465b
Jhulie <anajuliadamascenogaldino123@gmail.com>
2025-10-23 07:57:44 -0400

    Primeira atividade

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <lostream> // Substitui <stdio.h> para entrada e saída
+#include <cstdlib> // Substitui <stdlib.h> para std::system
+
+int main() {
+
+    //
+    std::cout <<"Oi! Mundo";
+
+    //
+    //
+    std::cout << "\n";
+    std::system("pause");
+
+    return 0;
+}
